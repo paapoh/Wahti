f594844052dabebc5e81c0c3d11fddb53a8ff016
paapoh <pohjolainenpaavo@gmail.com>
2022-03-06 11:58:42 +0200

    parser working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+from parser import parseUrl
 
 if __name__ == "__main__":
-    print("hello world")
+    lista = parseUrl('https://www.tori.fi/pirkanmaa?q=&cg=3020&w=1&st=s&st=k&st=u&st=h&st=g&c=3023&ps=&pe=&ca=11&l=0&md=th')
+    print(len(lista))
--- parser.py
+++ parser.py
@@ -1,14 +1,39 @@
-from ast import parse
-from tokenize import String
 from bs4 import BeautifulSoup
 import requests
 
-def parseUrl(url: String):
-    res = requests.get(url)
 
+class tori:
+    def __init__(self, title, image, id, price, link):
+        self.title = title
+        self.id = id
+        self.image = image
+        self.link = link
+        self.price = price
+        self.str_id = int(id[5:])
+
+    def __str__(self):
+        return f'{self.title} | {self.price} | {self.id}'
+
+
+def parseUrl(url: str):
+    res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
     items_div = soup.find_all('div', class_="list_mode_thumb")[0]
-    list_items = items_div.find_all('a')
-    print(list_items[0])
+    list_items = items_div.find_all('a', class_="item_row_flex")
 
-parseUrl('https://www.tori.fi/pirkanmaa?q=&cg=3020&w=1&st=s&st=k&st=u&st=h&st=g&c=3023&ps=&pe=&ca=11&l=0&md=th')
+    tori_list = []
+
+    for listing in list_items:
+
+        title = listing.find('div', class_="li-title").string
+        image = listing.find('img', class_="item_image")['src']
+        link = listing['href']
+        id = listing['id']
+        price = listing.find('p', class_="list_price ineuros").string
+        if(price is not None):
+            price = price[:-2]
+        
+        tori_list.append(
+            tori(title, image, id, price, link)
+        )
+    return tori_list
